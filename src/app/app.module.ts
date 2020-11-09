080e23830759eb8a8c471c2e9c485b81c2639bdd
BENROMDANE Mohamed Yassin <61516737+yassine-coder007@users.noreply.github.com>
2020-11-09 14:33:24 +0100

    tp2

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { AddProduitComponent } from './add-produit/add-produit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProduitsComponent,
+    AddProduitComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
